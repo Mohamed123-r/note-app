6a4e64523d4f1541ed815e1a1040714d8a46919e
mohamed <mezoattalla123@gmail.com>
2024-02-17 15:59:47 +0200

    change theme and create notes page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,9 +6,7 @@ class HomePae extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Notes"),
-      ),
+
     );
   }
 }
