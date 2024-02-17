fc4b8a79844c2842f373fb3a49c91ba8b941882b
mohamed <mezoattalla123@gmail.com>
2024-02-17 15:47:43 +0200

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePae extends StatelessWidget {
+  const HomePae({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+      ),
+    );
+  }
+}
