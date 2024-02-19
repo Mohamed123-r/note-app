b1a65668bcdc04b83e1c0ad69c41252c056baa04
mohamed <mezoattalla123@gmail.com>
2024-02-19 17:32:02 +0200

    pushing from the home page to the edit page

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,65 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/pages/edit_note_page.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(
-        top: 24,
-        bottom: 24,
-        left: 16,
-      ),
-      decoration: BoxDecoration(
-        color: const Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.only(right: 16,left: 16,),
-            title: const Text(
-              "First Note ",
-              style: TextStyle(
-                fontSize: 26,
-                color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNotePage();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(
+          top: 24,
+          bottom: 24,
+          left: 16,
+        ),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.only(
+                right: 16,
+                left: 16,
+              ),
+              title: const Text(
+                "First Note ",
+                style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16.0,
+                  bottom: 16,
+                ),
+                child: Text(
+                  "enter your first note enter your first note",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(0.4),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 36,
+                  color: Colors.black,
+                ),
               ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                bottom: 16,
-              ),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
               child: Text(
-                "enter your first note enter your first note",
+                "May22, 2024 ",
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 16,
                   color: Colors.black.withOpacity(0.4),
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 36,
-                color: Colors.black,
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              "May22, 2024 ",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withOpacity(0.4),
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
