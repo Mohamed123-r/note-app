acf6f41aaa4e0606817dd7149f2a18cb450f3309
mohamed <mezoattalla123@gmail.com>
2024-02-19 18:37:16 +0200

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class NoteModel {
+  final String title;
+  final String context;
+  final String date;
+  final Color color;
+
+  NoteModel({
+    required this.title,
+    required this.context,
+    required this.date,
+    required this.color,
+  });
+}
