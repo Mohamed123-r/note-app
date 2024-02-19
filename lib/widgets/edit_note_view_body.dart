24c4d6b8c0bbd48a8da52b0b7813f6f1e4cfe3d7
mohamed <mezoattalla123@gmail.com>
2024-02-19 18:24:41 +0200

    edit note page u i done

--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
+import 'package:note_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -21,6 +22,19 @@ class EditNoteViewBody extends StatelessWidget {
             CustomAppBar(
               title: 'Edit Note',
               icon: Icons.check,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomFormTextField(
+              labelText: 'Title',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomFormTextField(
+              labelText: 'Context',
+              maxLines: 5,
             ),
           ],
         ),
