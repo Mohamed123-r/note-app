f53ff5993be78832a347d7d21bf1f44ca03b48e9
mohamed <mezoattalla123@gmail.com>
2024-02-21 06:42:45 +0200

    add cubit to button sheet

--- lib/widgets/add_model_button_sheet.dart
+++ lib/widgets/add_model_button_sheet.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_states.dart';
 import 'add_note_form.dart';
-
 
 class AddModelButtonSheet extends StatelessWidget {
   const AddModelButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoadedState ? true : false,
+              child: const AddNoteForm(),
+            );
+          },
+          listener: (context, state) {
+            if (state is AddNoteFailureState) {
+              print("Failure ${state.errorMessage}");
+            }
+            if (state is AddNoteSuccessState) {
+              Navigator.pop(context);
+            }
+          },
+        ),
       ),
     );
   }
 }
-
-
