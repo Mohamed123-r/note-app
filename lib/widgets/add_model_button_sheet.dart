166c0cc171c3fb803e0060bfc70b2f7c71806c88
mohamed <mezoattalla123@gmail.com>
2024-02-22 07:17:58 +0200

    refactor multi bloc provider

--- lib/widgets/add_model_button_sheet.dart
+++ lib/widgets/add_model_button_sheet.dart
@@ -10,25 +10,28 @@ class AddModelButtonSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: BlocConsumer<AddNoteCubit, AddNoteState>(
-        builder: (context, state) {
-          return ModalProgressHUD(
-            inAsyncCall: state is AddNoteLoadedState ? true : false,
-            child: const SingleChildScrollView(
-              child: AddNoteForm(),
-            ),
-          );
-        },
-        listener: (context, state) {
-          if (state is AddNoteFailureState) {
-            print("Failure ${state.errorMessage}");
-          }
-          if (state is AddNoteSuccessState) {
-            Navigator.pop(context);
-          }
-        },
+    return BlocProvider(
+      create: (BuildContext context) =>AddNoteCubit(),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          builder: (context, state) {
+            return ModalProgressHUD(
+              inAsyncCall: state is AddNoteLoadedState ? true : false,
+              child: const SingleChildScrollView(
+                child: AddNoteForm(),
+              ),
+            );
+          },
+          listener: (context, state) {
+            if (state is AddNoteFailureState) {
+              debugPrint("Failure ${state.errorMessage}");
+            }
+            if (state is AddNoteSuccessState) {
+              Navigator.pop(context);
+            }
+          },
+        ),
       ),
     );
   }
