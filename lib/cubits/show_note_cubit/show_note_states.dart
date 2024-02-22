b43925f3d85387680f4f63ffeeb5f78f207cb692
mohamed <mezoattalla123@gmail.com>
2024-02-22 08:17:09 +0200

    create  show note states

---
+++ lib/cubits/show_note_cubit/show_note_states.dart
@@ -0,0 +1,19 @@
+import '../../models/note_model.dart';
+
+abstract class ShowNoteState {}
+
+class ShowNoteInitialState extends ShowNoteState {}
+
+class ShowNoteLoadedState extends ShowNoteState {}
+
+class ShowNoteSuccessState extends ShowNoteState {
+  ShowNoteSuccessState({required this.notes});
+
+  final List<NoteModel> notes;
+}
+
+class ShowNoteFailureState extends ShowNoteState {
+  final String errorMessage;
+
+  ShowNoteFailureState({required this.errorMessage});
+}
