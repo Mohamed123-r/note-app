3bd240c801afaeffe2cb09c3ad427f4d6e7550ef
mohamed <mezoattalla123@gmail.com>
2024-02-25 07:06:55 +0200

    pass data to edit note page

--- lib/pages/edit_note_page.dart
+++ lib/pages/edit_note_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/note_model.dart';
 
 import '../widgets/edit_note_view_body.dart';
 
 class EditNotePage extends StatelessWidget {
-  const EditNotePage({super.key});
-
+  const EditNotePage({super.key, required this.note});
+final NoteModel note;
   @override
   Widget build(BuildContext context) {
-    return const EditNoteViewBody();
+    return  EditNoteViewBody(note: note,);
   }
 }
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/widgets/custom_app_bar.dart';
 import 'package:note_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
-  const EditNoteViewBody({super.key});
-
+  const EditNoteViewBody({super.key, required this.note});
+final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
